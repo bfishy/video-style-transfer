85bac8a82a7dbeb6d169d07e1673d54a10825e95
byu14 <brandonyu1215@gmail.com>
2020-05-14 00:11:26 +0800

    comment out opt flow

--- model.py
+++ model.py
@@ -96,125 +96,123 @@ class Image(object):
 
         self.style_loss = sum(self.style_layer_w[i] * unweighted_loss[i] for i in range(num_layers))
 
-    def warp_image(self, img, flow):
-        h, w = flow.shape[:2]
-        flow = -flow
-        flow[:,:,0] += np.arange(w)
-        flow[:,:,1] += np.arange(h)[:,np.newaxis]
-        res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
-        return res
+    # def warp_image(self, img, flow):
+    #     h, w = flow.shape[:2]
+    #     flow = -flow
+    #     flow[:,:,0] += np.arange(w)
+    #     flow[:,:,1] += np.arange(h)[:,np.newaxis]
+    #     res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    #     return res
 
-    def make_opt_flow(self, prev, nxt):
-        prev = prev[0,:,:,:]
-        nxt = nxt[0,:,:,:]
-        prev = np.float32(prev)
-        nxt = np.float32(nxt)
-        prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-        nxt_gray = cv2.cvtColor(nxt, cv2.COLOR_BGR2GRAY)
-        flow = cv2.calcOpticalFlowFarneback(prev_gray, nxt_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        return flow
+    # def make_opt_flow(self, prev, nxt):
+    #     print(prev.shape)
+    #     print(nxt.shape)
+    #     prev_gray = cv2.cvtColor(np.float32(prev[0,:,:,:]), cv2.COLOR_BGR2GRAY)
+    #     nxt_gray = cv2.cvtColor(np.float32(nxt[0, :, :, :]), cv2.COLOR_BGR2GRAY)
+    #     flow = cv2.calcOpticalFlowFarneback(prev_gray, nxt_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    #     return flow
 
-    def get_prev_warped_frame(self, frame):
-        prev_frame = self.prev_frame
-        # backwards flow: current frame -> previous frame
-        flow = self.make_opt_flow(prev_frame, frame)
-        warped_img = self.warp_image(prev_frame, flow).astype(np.float32)
-        # img = preprocess(warped_img)
-        return warped_img
+    # def get_prev_warped_frame(self, frame):
+    #     prev_frame = self.prev_frame
+    #     # backwards flow: current frame -> previous frame
+    #     flow = self.make_opt_flow(prev_frame, frame)
+    #     warped_img = self.warp_image(prev_frame, flow).astype(np.float32)
+    #     # img = preprocess(warped_img)
+    #     return warped_img
 
-    #flow1 is back, flow2 is forward
-    def get_flow_weights(self, flow1, flow2): 
-        xSize = flow1.shape[1]
-        ySize = flow1.shape[0]
-        reliable = 255 * np.ones((ySize, xSize))
+    # #flow1 is back, flow2 is forward
+    # def get_flow_weights(self, flow1, flow2): 
+    #     xSize = flow1.shape[1]
+    #     ySize = flow1.shape[0]
+    #     reliable = 255 * np.ones((ySize, xSize))
 
-        size = xSize * ySize
+    #     size = xSize * ySize
 
-        x_kernel = [[-0.5, -0.5, -0.5],[0., 0., 0.],[0.5, 0.5, 0.5]]
-        x_kernel = np.array(x_kernel, np.float32)
-        y_kernel = [[-0.5, 0., 0.5],[-0.5, 0., 0.5],[-0.5, 0., 0.5]]
-        y_kernel = np.array(y_kernel, np.float32)
+    #     x_kernel = [[-0.5, -0.5, -0.5],[0., 0., 0.],[0.5, 0.5, 0.5]]
+    #     x_kernel = np.array(x_kernel, np.float32)
+    #     y_kernel = [[-0.5, 0., 0.5],[-0.5, 0., 0.5],[-0.5, 0., 0.5]]
+    #     y_kernel = np.array(y_kernel, np.float32)
         
-        flow_x_dx = cv2.filter2D(flow1[:,:,0],-1,x_kernel)
-        flow_x_dy = cv2.filter2D(flow1[:,:,0],-1,y_kernel)
-        dx = np.stack((flow_x_dx, flow_x_dy), axis = -1)
+    #     flow_x_dx = cv2.filter2D(flow1[:,:,0],-1,x_kernel)
+    #     flow_x_dy = cv2.filter2D(flow1[:,:,0],-1,y_kernel)
+    #     dx = np.stack((flow_x_dx, flow_x_dy), axis = -1)
 
-        flow_y_dx = cv2.filter2D(flow1[:,:,0],-1,x_kernel)
-        flow_y_dy = cv2.filter2D(flow1[:,:,0],-1,y_kernel)
-        dy = np.stack((flow_y_dx, flow_y_dy), axis = -1)
+    #     flow_y_dx = cv2.filter2D(flow1[:,:,0],-1,x_kernel)
+    #     flow_y_dy = cv2.filter2D(flow1[:,:,0],-1,y_kernel)
+    #     dy = np.stack((flow_y_dx, flow_y_dy), axis = -1)
 
-        motionEdge = np.zeros((ySize,xSize))
+    #     motionEdge = np.zeros((ySize,xSize))
 
-        for i in range(ySize):
-            for j in range(xSize): 
-                motionEdge[i,j] += dy[i,j,0]*dy[i,j,0]
-                motionEdge[i,j] += dy[i,j,1]*dy[i,j,1]
-                motionEdge[i,j] += dx[i,j,0]*dx[i,j,0]
-                motionEdge[i,j] += dx[i,j,1]*dx[i,j,1]
+    #     for i in range(ySize):
+    #         for j in range(xSize): 
+    #             motionEdge[i,j] += dy[i,j,0]*dy[i,j,0]
+    #             motionEdge[i,j] += dy[i,j,1]*dy[i,j,1]
+    #             motionEdge[i,j] += dx[i,j,0]*dx[i,j,0]
+    #             motionEdge[i,j] += dx[i,j,1]*dx[i,j,1]
             
 
-        for ax in range(xSize):
-            for ay in range(ySize): 
-                bx = ax + flow1[ay, ax, 0]
-                by = ay + flow1[ay, ax, 1]    
+    #     for ax in range(xSize):
+    #         for ay in range(ySize): 
+    #             bx = ax + flow1[ay, ax, 0]
+    #             by = ay + flow1[ay, ax, 1]    
 
-                x1 = int(bx)
-                y1 = int(by)
-                x2 = x1 + 1
-                y2 = y1 + 1
+    #             x1 = int(bx)
+    #             y1 = int(by)
+    #             x2 = x1 + 1
+    #             y2 = y1 + 1
                 
-                if x1 < 0 or x2 >= xSize or y1 < 0 or y2 >= ySize:
-                    reliable[ay, ax] = 0.0
-                    continue 
+    #             if x1 < 0 or x2 >= xSize or y1 < 0 or y2 >= ySize:
+    #                 reliable[ay, ax] = 0.0
+    #                 continue 
                 
-                alphaX = bx - x1 
-                alphaY = by - y1
+    #             alphaX = bx - x1 
+    #             alphaY = by - y1
 
-                a = (1.0-alphaX) * flow2[y1, x1, 0] + alphaX * flow2[y1, x2, 0]
-                b = (1.0-alphaX) * flow2[y2, x1, 0] + alphaX * flow2[y2, x2, 0]
+    #             a = (1.0-alphaX) * flow2[y1, x1, 0] + alphaX * flow2[y1, x2, 0]
+    #             b = (1.0-alphaX) * flow2[y2, x1, 0] + alphaX * flow2[y2, x2, 0]
                 
-                u = (1.0 - alphaY) * a + alphaY * b
+    #             u = (1.0 - alphaY) * a + alphaY * b
                 
-                a = (1.0-alphaX) * flow2[y1, x1, 1] + alphaX * flow2[y1, x2, 1]
-                b = (1.0-alphaX) * flow2[y2, x1, 1] + alphaX * flow2[y2, x2, 1]
+    #             a = (1.0-alphaX) * flow2[y1, x1, 1] + alphaX * flow2[y1, x2, 1]
+    #             b = (1.0-alphaX) * flow2[y2, x1, 1] + alphaX * flow2[y2, x2, 1]
                 
-                v = (1.0 - alphaY) * a + alphaY * b
-                cx = bx + u
-                cy = by + v
-                u2 = flow1[ay,ax,0]
-                v2 = flow1[ay,ax,1]
+    #             v = (1.0 - alphaY) * a + alphaY * b
+    #             cx = bx + u
+    #             cy = by + v
+    #             u2 = flow1[ay,ax,0]
+    #             v2 = flow1[ay,ax,1]
                 
-                if ((cx-ax) * (cx-ax) + (cy-ay) * (cy-ay)) >= 0.01 * (u2*u2 + v2*v2 + u*u + v*v) + 0.5: 
-                    # Set to a negative value so that when smoothing is applied the smoothing goes "to the outside".
-                    # Afterwards, we clip values below 0.
-                    reliable[ay, ax] = -255.0
-                    continue
+    #             if ((cx-ax) * (cx-ax) + (cy-ay) * (cy-ay)) >= 0.01 * (u2*u2 + v2*v2 + u*u + v*v) + 0.5: 
+    #                 # Set to a negative value so that when smoothing is applied the smoothing goes "to the outside".
+    #                 # Afterwards, we clip values below 0.
+    #                 reliable[ay, ax] = -255.0
+    #                 continue
                 
-                if motionEdge[ay, ax] > 0.01 * (u2*u2 + v2*v2) + 0.002: 
-                    reliable[ay, ax] = MOTION_BOUNDARIE_VALUE
-                    continue
+    #             if motionEdge[ay, ax] > 0.01 * (u2*u2 + v2*v2) + 0.002: 
+    #                 reliable[ay, ax] = MOTION_BOUNDARIE_VALUE
+    #                 continue
                 
-        #need to apply smoothing to reliable matrix
-        reliable = cv2.GaussianBlur(reliable,(3,3),0)
-        reliable = np.clip(reliable, 0.0, 255.0)    
-        return reliable
+    #     #need to apply smoothing to reliable matrix
+    #     reliable = cv2.GaussianBlur(reliable,(3,3),0)
+    #     reliable = np.clip(reliable, 0.0, 255.0)    
+    #     return reliable
     
-    def _temporal_loss(self, nxt, warped_prev, c):
-        D = tf.size(nxt, out_type = tf.float32)
-        loss = (1. / D) * tf.reduce_sum(tf.multiply(c, tf.squared_difference(nxt, warped_prev)))
-        loss = tf.cast(loss, tf.float32)
-        return loss
+    # def _temporal_loss(self, nxt, warped_prev, c):
+    #     D = tf.size(nxt, out_type = tf.float32)
+    #     loss = (1. / D) * tf.reduce_sum(tf.multiply(c, tf.squared_difference(nxt, warped_prev)))
+    #     loss = tf.cast(loss, tf.float32)
+    #     return loss
 
-    def sum_short_temporal_loss(self, frame):
-        prev_frame = self.prev_frame
-        if self.frame_idx == 0:
-            return 0
-        forward_flow = self.make_opt_flow(prev_frame, frame)
-        backward_flow = self.make_opt_flow(frame, prev_frame)
-        wp = self.warp_image(prev_frame)
-        c = self.get_flow_weights(backward_flow, forward_flow)
-        loss = self._temporal_loss(frame, wp, c)
-        return loss
+    # def sum_short_temporal_loss(self, frame):
+    #     prev_frame = self.prev_frame
+    #     if self.frame_idx == 0:
+    #         return 0
+    #     forward_flow = self.make_opt_flow(prev_frame, frame)
+    #     backward_flow = self.make_opt_flow(frame, prev_frame)
+    #     wp = self.warp_image(prev_frame)
+    #     c = self.get_flow_weights(backward_flow, forward_flow)
+    #     loss = self._temporal_loss(frame, wp, c)
+    #     return loss
 
     def loss(self):
         """
@@ -227,8 +225,8 @@ class Image(object):
                 gen_img_content = getattr(self.vgg, self.content_layer)
                 content_img_content = sess.run(gen_img_content)
             self._content_loss(content_img_content, gen_img_content)
-            # Temporal loss
-            self.temporal_loss = self.sum_short_temporal_loss(self.input_img)
+            # # Temporal loss
+            # self.temporal_loss = self.sum_short_temporal_loss(self.input_img)
 
             # Style loss
             with tf.compat.v1.Session() as sess:
@@ -237,7 +235,7 @@ class Image(object):
             self._style_loss(style_layers)
 
             # Total loss; update to self.total_loss for future optimization
-            self.total_loss = self.content_w * self.content_loss + self.style_w * self.style_loss + self.temporal_w * self.temporal_loss
+            self.total_loss = self.content_w * self.content_loss + self.style_w * self.style_loss
 
     def optimize(self):
         self.optimizer = train.AdamOptimizer(self.lr).minimize(self.total_loss, global_step=self.gstep)
@@ -269,25 +267,3 @@ class Image(object):
                     filepath = "./output/frame_%d.png" % self.frame_idx
                     save_image(filepath, gen_image)
                     return gen_image
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', required=True, help='path to the input')
-    parser.add_argument('-s', '--style_img', required=True, help='path to the style image')
-    # parser.add_argument('-o', '--output', required=True, help='output path')
-    parser.add_argument('--width', required=False, default=400, type=int, help='output image width')
-    parser.add_argument('--height', required=False, default=300, type=int, help='output image height')
-    parser.add_argument('--lr', required=False, default=2., type=float, help='hyperparameter: learning rate')
-    # parser.add_argument('--iter', required=False, default=250, type=int, help='hyperparameter: number of training iterations')
-
-    args = parser.parse_args()
-    input_path = args.input
-    style_path = args.style_img
-    img_height = args.height
-    img_width = args.width
-    lr = args.lr
-
-    model = Image(input_path, style_path, img_height, img_width, lr, 0)
-    model.build()
-    model.train(10)
